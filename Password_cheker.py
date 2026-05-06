220360ccd6a1a1d9a06deece0b478a898ffa69f2
Sravanthi <mudgantisaikeerthana@gmail.com>
2026-05-06 21:57:43 +0530

    Update Password_cheker.py

--- Password_cheker.py
+++ Password_cheker.py
@@ -1,3 +1,24 @@
 def check_password(password):
   length = len(password) >= 8
-  upper = any(c.isuppe
+  upper = any(c.isupper() for c in
+password)
+  lower = any(c.islower() for c in
+password)
+  digit = any(c.isdigit() for c in
+password)
+  special = any(c in "!@#$%^&*" for c in
+password)
+  score = sum([length, upper, lower, digit, sepecial])
+  if score == 5:
+      print("Strong Password 💪")
+  elif score >= 3:
+      print("Medium Password 👍")
+  else:
+      print("Week Password ❌")
+password = input("Enter Password: ")
+check_password(password)
+    
+    
+    
+
+              
