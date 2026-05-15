8d89982727bef2089ffd014ce5fe0078f2f91c27
Sravanthi <mudgantisaikeerthana@gmail.com>
2026-05-15 12:59:41 +0530

    Update memory_view_practice.py
    Signed-off-by: Sravanthi  <mudgantisaikeerthana@gmail.com>

--- memory_view_practice.py
+++ memory_view_practice.py
@@ -1,7 +1,7 @@
 data1 = bytes([65, 66, 67])
 mv1 = memoryview(data1)
 print("ASCII value at index 0 (b1ytes):", mv1[0])
-data2 = bytearyview(b"Python")
+data2 = bytearray(b"Python")
 mv2 = memoryview(data2)
 print("ASCII valaue of 'P' (bytearray):", mv2[0])
 mv2[0] = 74
