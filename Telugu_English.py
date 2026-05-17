082a2011a8f0720a3ff02f182cd48e70e468f2ed
Sravanthi <mudgantisaikeerthana@gmail.com>
2026-05-17 14:20:06 +0530

    Create Telugu_English.py
    Signed-off-by: Sravanthi  <mudgantisaikeerthana@gmail.com>

---
+++ Telugu_English.py
@@ -0,0 +1,6 @@
+telugu = int(input("Enter telugu marks: "))
+english = int(input("Enter English marks: "))
+total = telugu + english 
+for i in range(1, total + 1):
+    if total % i == 0:
+        print(f"{total} is divisible 
