2c54afda0514a81c1c1a1c36c01e9d07546b37d5
Sravanthi <mudgantisaikeerthana@gmail.com>
2026-05-17 14:38:02 +0530

    Update Telugu_English.py
    Signed-off-by: Sravanthi  <mudgantisaikeerthana@gmail.com>

--- Telugu_English.py
+++ Telugu_English.py
@@ -3,4 +3,6 @@ english = int(input("Enter English marks: "))
 total = telugu + english 
 for i in range(1, total + 1):
     if total % i == 0:
-        print(f"{total} is divisible 
+        print(f"{total} is divisible by {i}")
+else:
+    print("Loop completed! Final Total 
