93e6578e36bf96f5c22bb49d2678a3339cc92940
Sravanthi <mudgantisaikeerthana@gmail.com>
2026-05-20 16:45:08 +0530

    Update Vfx_assest_manager.py
    Signed-off-by: Sravanthi  <mudgantisaikeerthana@gmail.com>

--- Vfx_assest_manager.py
+++ Vfx_assest_manager.py
@@ -1,6 +1,9 @@
+
 import json
 imprt os
 class VFXAssest:
   """Class to represent a single VFX Asset (OOP Concept)"""
   def_init_(self, asset_id, asset_id, asset_name, asset_type, status="In Progress"):
-  
+     self.asset_id = asset_id
+     self.asset_name = asset_name
+
