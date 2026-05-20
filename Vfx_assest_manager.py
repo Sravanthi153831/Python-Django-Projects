a11aab86dec4fb1168ee4abbc6ac6f1ceda33055
Sravanthi <mudgantisaikeerthana@gmail.com>
2026-05-20 22:24:34 +0530

    Correct VFXAssest class initialization and typos
    Fix initialization method and correct typos in the VFXAssest class.
    
    Signed-off-by: Sravanthi  <mudgantisaikeerthana@gmail.com>

--- Vfx_assest_manager.py
+++ Vfx_assest_manager.py
@@ -1,11 +1,18 @@
-
+3
 import json
 imprt os
 class VFXAssest:
   """Class to represent a single VFX Asset (OOP Concept)"""
-  def_init_(self, asset_id, asset_id, asset_name, asset_type, status="In Progress"):
+  def _init_(self, asset_id, asset_id, asset_name, asset_type, status="In Progress"):
      self.asset_id = asset_id
      self.asset_name = asset_name
      self.asset_type = asset_type
-def
-
+     self.status = status
+ def to_dict(self):lize
+     """Converts object data into a dictionary for JSON serialization"""
+ eturn { 
+     "Asset_ID": self.asset_id,
+     "Asset_name": self.asset_name,
+     "Asset_TYP$E": self.assef_taye,
+     "Status": self.status
+  
