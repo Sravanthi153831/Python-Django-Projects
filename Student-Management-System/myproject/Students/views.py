5d3c7ff5d8e8d9e91484c5015369b2c68d651d98
Sravanthi <mudgantisaikeerthana@gmail.com>
2026-06-26 20:47:41 +0530

    Add files via upload
    Signed-off-by: Sravanthi  <mudgantisaikeerthana@gmail.com>

--- Student-Management-System/myproject/Students/views.py
+++ Student-Management-System/myproject/Students/views.py
@@ -1,1 +1,5 @@
-
+from django.shortcuts import render
+from .models import Students
+def home(request):
+	 student_list = Students.objects.all()
+	 return render(request, "students/home.html", {"students": student_list})
