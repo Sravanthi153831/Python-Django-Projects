5ed5dd922a91f95fd90fb147d0e83f09d47c72e7
Sravanthi <mudgantisaikeerthana@gmail.com>
2026-07-03 21:53:57 +0530

    Add files via upload
    Signed-off-by: Sravanthi  <mudgantisaikeerthana@gmail.com>

--- Flask-CRUD-API/app.py
+++ Flask-CRUD-API/app.py
@@ -1,1 +1,28 @@
+from flask import Flask, jsonify, request
 
+app = Flask(__name__)
+
+# Initial sample data
+cars = [
+    {"id": 1, "name": "Tata Nexon"},
+    {"id": 2, "name": "Mahindra XUV700"}
+]
+
+# 1. GET Method - To view all cars
+@app.route('/cars', methods=['GET'])
+def get_cars():
+    return jsonify(cars)
+
+# 2. POST Method - To add a new car
+@app.route('/cars', methods=['POST'])
+def add_car():
+    new_car_data = request.get_json()
+    cars.append(new_car_data)
+    return jsonify({
+        "status": "success",
+        "message": "Car added successfully!",
+        "all_cars": cars
+    }), 201
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
